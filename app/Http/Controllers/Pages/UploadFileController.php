e6e576d6e0b3db712cc0be27bc35610e45c570a9
kennykarnama <kennykarnama@gmail.com>
2018-01-28 14:49:00 +0700

    apalgi

--- app/Http/Controllers/Pages/UploadFileController.php
+++ app/Http/Controllers/Pages/UploadFileController.php
@@ -16,6 +16,16 @@ class UploadFileController extends Controller
     public function indexHome()
     {
     	# code...
+    	ini_set('memory_limit', '-1');
+
+    		ini_set('upload_max_filesize', '10M');
+			
+			ini_set('post_max_size', '10M');
+
+			ini_set('max_input_time', 300);
+			
+			ini_set('max_execution_time', 300);
+			
     	return view('pages.upload_file',[]);
     }
 
